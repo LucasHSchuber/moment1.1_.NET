014359676809e375fc29dac7fa05df9a5a0df70f
Lucas Schuber <lucas.hammarstrand@hotmail.com>
2024-01-20 07:13:41 +0100

    first commit

---
+++ Pages/Razor.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Moment1._1_razor.Pages
+{
+    public class RazorModel : PageModel
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        public RazorModel[] DataArray { get; set; }
+
+        public void OnGet()
+        {
+            // Initialize the array within a method (OnGet)
+            DataArray = new RazorModel[]
+            {
+                new RazorModel { Code = "Webbutveckling I", Name = "DT057G" },
+                new RazorModel { Code = "Webbutveckling II", Name = "DT093G" },
+                 new RazorModel { Code = "Webbutveckling III", Name = "DT173G" },
+                new RazorModel { Code = "Fullstackutveckling med ramverk", Name = "DT071G" },
+                 new RazorModel { Code = "Programmering i C#.NET", Name = "DT193G" },
+                  new RazorModel { Code = "Databaser", Name = "DT003G" },
+
+            };
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+// let x = document.getElementById("toggler");
+// addEventListener.x(click, openNav)
+
+
+function openNav() {
+    if (document.getElementById("sidebar").style.width === "100%") {
+        document.getElementById("sidebar").style.width = "0";
+    } else {
+        document.getElementById("sidebar").style.width = "100%";
+    }
+}
+
+
+function closeNav(){
+    document.getElementById("sidebar").style.width = "0";
+}
